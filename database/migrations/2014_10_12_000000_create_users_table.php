95df3d32107c0c7331fa219535e755be84232748
cahindo <erikfaderik@gmail.com>
2021-05-28 20:50:22 +0700

    benakne eror

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,24 +17,11 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('role');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-<<<<<<< HEAD
-            $table->string('role')->nullable();
-
-            // $table->string('no_hp')->nullable();
-            // $table->string('alamat')->nullable();
-            // $table->string('kelurahan')->nullable();
-            // $table->string('kecamatan')->nullable();
-            // $table->string('kota')->nullable();
-            // $table->rememberToken();
-            // $table->foreignId('current_team_id')->nullable();
-            // $table->text('profile_photo_path')->nullable();
-            // $table->timestamps();
-=======
             $table->rememberToken();
             $table->timestamps();
->>>>>>> 076c2d4ec84dac0fe7f86c8fa73abde2a2ec6f66
         });
     }
 
