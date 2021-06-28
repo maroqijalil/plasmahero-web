c4722555358c225b0c775f413ea10a550f978fbb
Allam Taju Sarof <55141556+Allam0053@users.noreply.github.com>
2021-06-28 11:53:09 +0700

    fix:rewrite-api.php

--- routes/api.php
+++ routes/api.php
@@ -22,14 +22,6 @@ use App\Common\Controllers\Auth\RegisteredUserController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-// Route::get('/products', [ProductController::class, 'index']);
-// Route::get('/products/{id}', [ProductController::class, 'show']);
-// Route::get('/products/search/{name}', [ProductController::class, 'search']);
-// Route::post('/products', [ProductController::class, 'store']);
-// Route::put('/products/{id}', [ProductController::class, 'update']);
-// Route::delete('/products/{id}', [ProductController::class, 'destroy']);
-
 // CREATE
 // Public routes
 Route::get('/menu', [BaseController::class, 'availableMenu']);
@@ -38,6 +30,10 @@ Route::post('/login', [AuthController::class, 'login']);
 
 // Protected routes
 Route::group(['middleware' => ['auth:sanctum']], function () {
+    
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', [RegisteredUserController::class, 'fetch']);
+
     Route::group(['prefix' => 'my-profile'], function () {
         Route::get('', [ProfileController::class, 'index']);
         Route::post('', [ProfileController::class, 'store']);
@@ -54,35 +50,25 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
         Route::delete('/{id}', [PendonoranController::class, 'destroy']);
     });
 
+    Route::group(['prefix' => 'gallery'], function () {
+        Route::post('', [GalleryController::class, 'create']);
+        Route::get('', [GalleryController::class, 'fetch']);
+        Route::put('/{id}', [GalleryController::class, 'update']);
+        Route::delete('/{id}', [GalleryController::class, 'destroy']);
+    });
 
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/user', [RegisteredUserController::class, 'fetch']);
-    // Route::get('/donor', [DonorController::class, 'fetch']);
-    // Route::get('/pendonoran', [PendonoranController::class, 'fetch']);
-
-    Route::post('/gallery', [GalleryController::class, 'create']);
-    Route::get('/gallery', [GalleryController::class, 'fetch']);
-    Route::put('/gallery/{id}', [GalleryController::class, 'update']);
-    Route::delete('/gallery/{id}', [GalleryController::class, 'destroy']);
-
-    Route::post('/report', [ReportController::class, 'create']);
-    Route::get('/report', [ReportController::class, 'fetch']);
-    Route::put('/report/{id}', [ReportController::class, 'update']);
-    Route::delete('/report/{id}', [ReportController::class, 'destroy']);
-
-    Route::get('/donor', [DonorController::class, 'index']);
-    Route::get('/donor/{id}', [DonorController::class, 'show']);
-    Route::post('/donor', [DonorController::class, 'store']);
-    Route::put('/donor/{id}', [DonorController::class, 'update']);
-    Route::delete('/donor/{id}', [DonorController::class, 'destroy']);
-
-});
-
-Route::get('/user', [RegisteredUserController::class, 'fetch']);
-Route::get('/report', [ReportController::class, 'fetch']);
-Route::get('/gallery', [GalleryController::class, 'fetch']);
-Route::get('/pendonoran', [PendonoranController::class, 'fetch']);
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::group(['prefix' => 'report'], function () {
+        Route::post('', [ReportController::class, 'create']);
+        Route::get('', [ReportController::class, 'fetch']);
+        Route::put('/{id}', [ReportController::class, 'update']);
+        Route::delete('/{id}', [ReportController::class, 'destroy']);
+    });
+    
+    Route::group(['prefix' => 'donor'], function () {
+        Route::get('', [DonorController::class, 'index']);
+        Route::get('/{id}', [DonorController::class, 'show']);
+        Route::post('', [DonorController::class, 'store']);
+        Route::put('/{id}', [DonorController::class, 'update']);
+        Route::delete('/{id}', [DonorController::class, 'destroy']);
+    });
 });
