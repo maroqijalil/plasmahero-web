d54aa70c67db16593d440a8df1ee1e9533c8005b
Iwan Dwi Prakoso <iwan.dprakoso@gmail.com>
2021-07-05 12:48:28 +0700

    style: minor

--- resources/views/admin/donor/laporan-tanggal.blade.php
+++ resources/views/admin/donor/laporan-tanggal.blade.php
@@ -32,7 +32,7 @@
                             <th scope="col" class="sort" data-sort="judul">Penerima Donor</th>
                             <th scope="col" class="sort" data-sort="judul">Tanggal Pendonoran</th>
                             <th scope="col" class="sort" data-sort="deskripsi">Tempat</th>
-                            <th scope="col"></th>
+{{--                            <th scope="col"></th>--}}
                         </tr>
                         </thead>
                         <tbody class="list">
@@ -96,31 +96,31 @@
                         </tbody>
                     </table>
                 </div>
-                <div class="card-footer py-4">
-                    <nav aria-label="...">
-                        <ul class="pagination justify-content-end mb-0">
-                            <li class="page-item disabled">
-                                <a class="page-link" href="#" tabindex="-1">
-                                    <i class="fas fa-angle-left"></i>
-                                    <span class="sr-only">Previous</span>
-                                </a>
-                            </li>
-                            <li class="page-item active">
-                                <a class="page-link" href="#">1</a>
-                            </li>
-                            <li class="page-item">
-                                <a class="page-link" href="#">2 <span class="sr-only">(current)</span></a>
-                            </li>
-                            <li class="page-item"><a class="page-link" href="#">3</a></li>
-                            <li class="page-item">
-                                <a class="page-link" href="#">
-                                    <i class="fas fa-angle-right"></i>
-                                    <span class="sr-only">Next</span>
-                                </a>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
+{{--                <div class="card-footer py-4">--}}
+{{--                    <nav aria-label="...">--}}
+{{--                        <ul class="pagination justify-content-end mb-0">--}}
+{{--                            <li class="page-item disabled">--}}
+{{--                                <a class="page-link" href="#" tabindex="-1">--}}
+{{--                                    <i class="fas fa-angle-left"></i>--}}
+{{--                                    <span class="sr-only">Previous</span>--}}
+{{--                                </a>--}}
+{{--                            </li>--}}
+{{--                            <li class="page-item active">--}}
+{{--                                <a class="page-link" href="#">1</a>--}}
+{{--                            </li>--}}
+{{--                            <li class="page-item">--}}
+{{--                                <a class="page-link" href="#">2 <span class="sr-only">(current)</span></a>--}}
+{{--                            </li>--}}
+{{--                            <li class="page-item"><a class="page-link" href="#">3</a></li>--}}
+{{--                            <li class="page-item">--}}
+{{--                                <a class="page-link" href="#">--}}
+{{--                                    <i class="fas fa-angle-right"></i>--}}
+{{--                                    <span class="sr-only">Next</span>--}}
+{{--                                </a>--}}
+{{--                            </li>--}}
+{{--                        </ul>--}}
+{{--                    </nav>--}}
+{{--                </div>--}}
             </div>
         </div>
     </div>
